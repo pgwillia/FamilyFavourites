414c22970c59a8c5949d59146323dfa674b9ecf8
Tricia Jenkins <tricia.g.jenkins@gmail.com>
2018-12-17 00:07:19 -0700

    add index to users email to aid find and uniqueness

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181217052233) do
+ActiveRecord::Schema.define(version: 20181217070116) do
 
   create_table "recipes", force: :cascade do |t|
     t.string "source"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20181217052233) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
