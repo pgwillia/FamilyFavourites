820207c612db661126f16252a59d04caea764952
Tricia Jenkins <tricia.g.jenkins@gmail.com>
2018-12-16 23:05:47 -0700

    WIP add user

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   resources :recipes
 
   root 'recipes#index'
--- db/schema.rb
+++ db/schema.rb
@@ -10,20 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_181_213_154_242) do
-  create_table 'recipes', force: :cascade do |t|
-    t.string 'source'
-    t.text 'ingredients'
-    t.text 'method'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.string 'title'
+ActiveRecord::Schema.define(version: 20181217052233) do
+
+  create_table "recipes", force: :cascade do |t|
+    t.string "source"
+    t.text "ingredients"
+    t.text "method"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "title"
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'name'
-    t.string 'email'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
+
 end
