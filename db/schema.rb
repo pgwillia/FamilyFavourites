2f30137208da10e012f07fa653b7c0aed714934f
Tricia Jenkins <tricia.g.jenkins@gmail.com>
2018-12-17 00:31:33 -0700

    add password to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181217070116) do
+ActiveRecord::Schema.define(version: 20181217070742) do
 
   create_table "recipes", force: :cascade do |t|
     t.string "source"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20181217070116) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
